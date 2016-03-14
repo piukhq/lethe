4e5c3c82a8fc0d9baac891fc39007261847ebada
Chris Latham <cl@loyaltyangels.com>
2016-03-14 11:38:49 +0000

    don't put a forward slash on the end of URLs if no args are passed to url_join

--- app/utils.py
+++ app/utils.py
@@ -3,5 +3,8 @@ from urllib.parse import urljoin
 
 
 def url_for(endpoint, *args):
-    arg_string = '/'.join(args)
-    return urljoin(BASE_URL, '{}/{}'.format(endpoint, arg_string))
+    if args is None:
+        return urljoin(BASE_URL, endpoint)
+    else:
+        arg_string = '/'.join(args)
+        return urljoin(BASE_URL, '{}/{}'.format(endpoint, arg_string))
