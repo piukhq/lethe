573d2963dede99c3e19c411bfd4b9b5247951c8e
Chris Latham <cl@loyaltyangels.com>
2016-03-14 10:55:51 +0000

    use relative urls in redirects

--- app/tests/test_utils.py
+++
@@ -1,9 +0,0 @@
-import unittest
-
-from app.utils import url_for
-
-
-class TestUtils(unittest.TestCase):
-    def test_url_for(self):
-        url = url_for('/test_url', 'test_arg1', 'test_arg2')
-        self.assertTrue(url.endswith('/test_url/test_arg1/test_arg2'))
--- app/utils.py
+++
@@ -1,7 +0,0 @@
-from settings import BASE_URL
-from urllib.parse import urljoin
-
-
-def url_for(endpoint, *args):
-    arg_string = '/'.join(args)
-    return urljoin(BASE_URL, '{}/{}'.format(endpoint, arg_string))
