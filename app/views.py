64a7c079686f746d97952b7ab5d28a36dde3e46f
olivere <oe@bink.com>
2017-03-14 11:19:53 +0000

    Redirect to link expired error if user attempts to use expired link for password reset twice or more.

--- app/views.py
+++ app/views.py
@@ -41,9 +41,7 @@ def new_password(link_token=None):
             flash(err)
             return redirect(url_for('frontend.new_password', link_token=link_token))
         else:
-            flash('Sorry, either your link has expired or something has gone wrong on our end. '
-                  'Give us some time to fix it, and try again later!')
-            return render_template('error_page.html')
+            return render_template('link_expired.html')
     else:
         try:
             if is_valid_token(link_token):
