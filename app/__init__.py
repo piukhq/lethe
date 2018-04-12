02dd26e2a16fa91d443f14ee08117852dd42e7fb
Chris Latham <backwardspy@gmail.com>
2018-04-12 17:14:06 +0100

    make static url configurable

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,15 @@
 from flask import Flask
 
+import settings
+
 
 def create_app(config_name="settings"):
     from app.views import frontend
 
-    app = Flask(__name__, static_folder='password')
+    app = Flask(
+        __name__,
+        static_folder='static',
+        static_url_path=settings.STATIC_URL)
     app.config.from_object(config_name)
     app.register_blueprint(frontend)
 
