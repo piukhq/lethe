3478e97c538d0d34567659620897ff69602c74c3
Terry Cain <tcain@bink.com>
2020-11-26 10:06:41 +0000

    Cleaned up env vars, fixed deprectation in manage.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,15 @@
-from sentry_sdk.integrations.flask import FlaskIntegration
-from flask import Flask
-from flask_cdn import CDN
+import os
+
 import sentry_sdk
+from flask import Flask
+from sentry_sdk.integrations.flask import FlaskIntegration
 
 import settings
-
-cdn = CDN()
 
 
 def create_app(config_name='settings'):
     from app.views import frontend, internal
+    from app.version import __version__
 
     app = Flask(
         __name__,
@@ -20,17 +20,12 @@ def create_app(config_name='settings'):
     app.register_blueprint(internal)
     app.register_blueprint(frontend)
 
-    cdn.init_app(app)
 
-    if app.config.get('SENTRY_DSN'):
-        from app.version import __version__
-        sentry_sdk.init(
-            dsn=app.config['SENTRY_DSN'],
-            environment=app.config['SENTRY_ENV'],
-            integrations=[
-                FlaskIntegration()
-            ],
-            release=__version__
-        )
+    sentry_sdk.init(
+        integrations=[
+            FlaskIntegration()
+        ],
+        release=__version__
+    )
 
     return app
