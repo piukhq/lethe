b4b1b52f6a47f932696acc0aacc1018bcaddf3cc
Francesco Milani <fmilani@bink.com>
2021-02-09 16:59:57 +0200

    update comment

--- prometheus/metrics.py
+++ prometheus/metrics.py
@@ -3,7 +3,7 @@ from prometheus_client import Counter, CollectorRegistry
 registry = CollectorRegistry()
 
 # prometheus will add _total at the end of the metric name if its a Counter
-# so this metric full name will be lethe_requests_by_status_total
+# so this metric full name will be lethe_request_by_template_total
 request_counter = Counter(
     name="request_by_template",
     documentation="Total requests received by name of the returned template.",
