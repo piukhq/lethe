959a6aed6de969a12a25b06b11d97052ab386161
simontunmore <stunmore@bink.com>
2021-06-28 15:53:36 +0100

    Implemented primary font and colour overrides - changed over images.

--- settings.py
+++ settings.py
@@ -19,7 +19,7 @@ DEV_PORT = env_var("DEV_PORT", 5000)
 DEV_HOST = env_var("DEV_HOST", "127.0.0.1")
 
 BASE_URL = env_var("LETHE_URL", "http://{}:{}".format(DEV_HOST, DEV_PORT))
-HERMES_URL = env_var("HERMES_URL", "http://dev.hermes.loyaltyangels.local")
+HERMES_URL = env_var("HERMES_URL", "http://api.dev.gb.bink.com")
 
 # this will be prefixed onto any redirects sent to the user.
 # if set, it must include the http scheme (probably http:// or https://), and it must not end in a forward slash.
