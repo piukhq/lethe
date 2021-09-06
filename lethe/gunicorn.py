6ef9b9c992c4c4a3e78108048c267c803d204f91
Terry Cain <tcain@bink.com>
2021-09-06 16:43:47 +0100

    Added metrics to url filter

--- lethe/gunicorn.py
+++ lethe/gunicorn.py
@@ -8,7 +8,7 @@ import gunicorn.glogging
 class HealthcheckFilter(logging.Filter):
     def filter(self, record: logging.LogRecord) -> bool:
         msg = record.getMessage()
-        return "/healthz" not in msg and "/readyz" not in msg and "/livez" not in msg
+        return "/healthz" not in msg and "/readyz" not in msg and "/livez" not in msg and "/metrics" not in msg
 
 
 class Logger(gunicorn.glogging.Logger):
